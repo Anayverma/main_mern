e958bd765315e16b73a0f88702c08c8615e4e0d2
Anay Verma <anayverma2147@gmail.com>
2024-01-25 10:32:47 +0530

    mongodb connection setup

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+;const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log("\n from db/index.js    MongoDb connected !!!!  DB Host --",connectionInstance.connection.host)
+    } catch (error) {
+        console.error("Mongodb error -- in db/index.js ",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,46 @@
+// require('dotenv').config({ path: './env' });
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config(
+    {
+        path: "./env"
+    }
+)
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;(async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     } catch (error) {
+//         console.error("ERROR :",error)
+//         throw error
+//     }
+// })()
