dacf95cfa56400f8691fb415b3db0b5ed8789ff1
Anay Verma <anayverma2147@gmail.com>
2024-01-25 16:18:42 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from"express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express() 
+
+app.use(cors( {
+    origin: process.env.CORS , 
+    credentials: true ,
+}))
+
+app.use(express.json({
+    limit: "16kb"
+}))
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({
+    extended: true, 
+    limit: "16kb"
+}))
+
+app.use(cookieParser())
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
 // require('dotenv').config({ path: './env' });
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
 
-
-dotenv.config(
-    {
-        path: "./env"
-    }
-)
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    const port = process.env.PORT || 8000;
 
+    app.on("error", (error) => {
+        console.log("util/index.js error -----",error)
+        process.exit(1)
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.listen(port, () => {
+      console.log("utils/index.js   server running at port", port);
+    });
+  })
+  .catch((err) => {
+    console.log(" util/index.js  MONGO DB connection failed   ---  ");
+  });
 
 // ;(async()=>{
 //     try {
